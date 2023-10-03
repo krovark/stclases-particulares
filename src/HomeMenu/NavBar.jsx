a13cc91ceee9c7081d4520ba905fb2329d315599
Matias Guazzolini <matiguazzo@gmail.com>
2023-10-02 21:10:56 -0300

    Arrelgo navbar sin registro

--- src/HomeMenu/NavBar.jsx
+++ src/HomeMenu/NavBar.jsx
@@ -37,10 +37,10 @@ const NavigationBar = () => {
           {/* Menú de navegación centrado */}
           <Nav className="mx-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
             {/* Enlaces de inicio, perfil y historial */}
-            <Nav.Link as={Link} to="/home" className="text-center" style={{ color: 'white' }}>Home</Nav.Link>
 
             {isLoggedIn && ( // mostrar estos enlaces solo si el usuario está autenticado
           <>
+            <Nav.Link as={Link} to="/home" className="text-center" style={{ color: 'white' }}>Home</Nav.Link>
             <Nav.Link as={Link} to="/perfil" className="text-center" style={{ color: 'white' }}>Perfil</Nav.Link>
             <Nav.Link as={Link} to="/historial" className="text-center" style={{ color: 'white' }}>Historial</Nav.Link>
 
